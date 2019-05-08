6ee81f4173ed5a7d9ae42e8f02259238c7f64ba2
newtom69 <thomas@vuille.fr>
2019-05-09 00:02:05 +0200

    delete not used sections

--- TryPdfClown/Class1.cs
+++ TryPdfClown/Class1.cs
@@ -46,9 +46,7 @@ namespace Test1
             foreach (Page page in document.Pages)
             {
                 stamper.Page = page;
-
                 Extract(new ContentScanner(page), page);
-
                 stamper.Flush();
             }
         }
@@ -65,40 +63,10 @@ namespace Test1
 
                 string aa = content.GetType().ToString();
 
-                //if (content is Text)
-                //{
-                //    ContentScanner.TextWrapper text = (ContentScanner.TextWrapper)level.CurrentWrapper;
-                //    //ContentScanner.GraphicsState test = level.getState();
-                //    foreach (ContentScanner.TextStringWrapper textString in text.TextStrings)
-                //    {
-                //        org.pdfclown.objects.Rectangle rf = (RectangleF)textString.Box;
-                //        /*txtOutput.Text = txtOutput.Text + Environment.NewLine + "Text [font size: " + textString.Style.FontSize + " ],[font Name: " +
-                //            textString.Style.Font.Name + " ]: " + textString.Text + "[position = left :" + rf.Left.ToString() + " & Top: " + rf.Top.ToString() + "X:" + rf.X.ToString() + "Y:" + rf.Y.ToString();*/
-
-                //        txtOutput.Text = txtOutput.Text + Environment.NewLine + textString.Text;
-
-                //    }
-
-                //}
-
-                //else if (content is ShowText)
-                //{
-                //    it.stefanochizzolini.clown.documents.contents.fonts.Font font = level.State.Font;
-                //    txtOutput.Text = txtOutput.Text + (font.Decode(((ShowText)content).Text));
-
-                //}
-
                 if (content is ContainerObject)
                 {
-                    // Scan the inner level!
                     Extract(level.ChildLevel, page);
                 }
-                else if (content is InlineImage)
-                {
-                    ContentScanner.InlineImageWrapper img = (ContentScanner.InlineImageWrapper)level.CurrentWrapper;
-                   // ExportImage(img.InlineImage.Body.Value, System.IO.Path.DirectorySeparatorChar + "ImageExtractionSample_" + (index++) + ".jpg");
-                }
-
                 else if (content is GraphicsObject)
                 {
 
@@ -107,10 +75,7 @@ namespace Test1
                     {
                         continue;
                     }
-
-
                     SizeF? imageSize = null; // Image native size.
-
                     if (objectWrapper is ContentScanner.XObjectWrapper)
                     {
                         ContentScanner.XObjectWrapper xObjectWrapper = (ContentScanner.XObjectWrapper)objectWrapper;
@@ -118,91 +83,28 @@ namespace Test1
                         // Is the external object an image?
                         if (xobject is ImageXObject)
                         {
-
                             imageSize = xobject.Size; // Image native size.
-
                             PdfDataObject dataObject = xobject.BaseDataObject;
-                            ;
                             PdfDictionary header = ((PdfStream)dataObject).Header;
                             if (header.ContainsKey(PdfName.Type) && header[PdfName.Type].Equals(PdfName.XObject) && header[PdfName.Subtype].Equals(PdfName.Image))
                             {
-                                if (header[PdfName.Filter].Equals(PdfName.Image)) // JPEG image.
-                                {
-                                    // Get the image data (keeping it encoded)!
-                                    IBuffer body1 = ((PdfStream)dataObject).GetBody(false);
-                                    //object h1 = PdfName.ColorSpace;
-
-
-
-                                    // Export the image!
-
-                                    ExportImage( body1, System.IO.Path.DirectorySeparatorChar + "Image_" + (index++) + ".png"
-                                      );
-                                }
+                                IBuffer body1 = ((PdfStream)dataObject).GetBody(false);
+                                ExportImage(body1, @"D:\" + "Image_" + (index++) + ".png");
                             }
-
-
-
                         }
-
-
-                        //else if (content is it.stefanochizzolini.clown.documents.interaction.annotations.Link)
-                        //{
-                        //    Dictionary<RectangleF?, List<ITextString>> textStrings = null;
-                        //    PageAnnotations annotations = page.Annotations;
-                        //    TextExtractor extractor = new TextExtractor();
-
-                        //    if (annotations == null)
-                        //    {
-                        //        Console.WriteLine("No annotations here.");
-                        //        continue;
-                        //    }
-
-                        //    foreach (it.stefanochizzolini.clown.documents.interaction.annotations.Annotation annotation in annotations)
-                        //    {
-                        //        if (annotation is it.stefanochizzolini.clown.documents.interaction.annotations.Link)
-                        //        {
-
-                        //            if (textStrings == null)
-                        //            { textStrings = extractor.Extract(page); }
-
-                        //            it.stefanochizzolini.clown.documents.interaction.annotations.Link link = (it.stefanochizzolini.clown.documents.interaction.annotations.Link)annotation;
-                        //            RectangleF linkBox = link.Box;
-                        //            StringBuilder linkTextBuilder = new StringBuilder();
-                        //            foreach (ITextString linkTextString in extractor.Filter(textStrings, linkBox))
-                        //            { linkTextBuilder.Append(linkTextString.Text); }
-                        //            string bb = linkTextBuilder.ToString();
-                        //            txtOutput.Text = txtOutput.Text + "Link '" + linkTextBuilder.ToString();
-                        //            txtOutput.Text = txtOutput.Text + "    Position: "
-                        //                + "x:" + Math.Round(linkBox.X) + ","
-                        //                + "y:" + Math.Round(linkBox.Y) + ","
-                        //                + "w:" + Math.Round(linkBox.Width) + ","
-                        //                + "h:" + Math.Round(linkBox.Height);
-
-                        //        }
-
-                        //    }
                     }
-
-
                     else if (objectWrapper is ContentScanner.InlineImageWrapper)
                     {
                         InlineImage inlineImage = ((ContentScanner.InlineImageWrapper)objectWrapper).InlineImage;
                         imageSize = inlineImage.Size; // Image native size.
-
                     }
-
                     if (imageSize.HasValue)
                     {
                         RectangleF box = objectWrapper.Box.Value; // Image position (location and size) on the page.
                     }
-
-                    /*=============================================================================*/
                 }
-
             }
         }
-
         private void ExportImage(IBuffer data, string outputPath)
         {
             FileStream outputStream;
@@ -225,39 +127,6 @@ namespace Test1
             {
                 throw new Exception(outputPath + " file writing has failed.", e);
             }
-
-            Console.WriteLine("Output: " + outputPath);
         }
-
-
-        //private byte[] BmpToBytes_Unsafe(Bitmap bmp)
-        //{
-
-        //    BitmapData bData = bmp.LockBits(new System.Drawing.Rectangle(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT),
-        //        ImageLockMode.ReadOnly,
-        //        PixelFormat.Format24bppRgb);
-
-        //    int lineSize = bData.Width * 3;
-        //    int byteCount = lineSize * bData.Height;
-        //    byte[] bmpBytes = new byte[byteCount];
-        //    byte[] tempLine = new byte[lineSize];
-        //    int bmpIndex = 0;
-
-        //    IntPtr scan = new IntPtr(bData.Scan0.ToInt32() + (lineSize * (bData.Height - 1)));
-
-        //    for (int i = 0; i < bData.Height; i++)
-        //    {
-        //        Marshal.Copy(scan, tempLine, 0, lineSize);
-        //        scan = new IntPtr(scan.ToInt32() - bData.Stride);
-        //        tempLine.CopyTo(bmpBytes, bmpIndex);
-        //        bmpIndex += lineSize;
-        //    }
-
-        //    bmp.UnlockBits(bData);
-
-        //    return bmpBytes;
-        //}
     }
-
-
 }
