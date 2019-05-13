2f82bac4207b4ef3d8f0ecc2554ae248e8c7dfda
newtom69 <thomas@vuille.fr>
2019-05-13 23:56:21 +0200

    test open zip file

---
+++ OpenCbz/main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCbz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipArchive zip;
+            MemoryStream memoryStream;
+            try
+            {
+                zip = ZipFile.Open("test.cbr", ZipArchiveMode.Read);
+                foreach (var entrie in zip.Entries)
+                {
+                    Console.WriteLine(entrie.FullName);
+                    memoryStream = (MemoryStream)entrie.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+
+
+
+        }
+    }
+}
