70035fb0bbae0f948e2eb3e829f1a3de4fe27dc4
newtom69 <thomas@vuille.fr>
2019-05-23 16:34:04 +0200

    SharpCompress test open zip ok

--- OpenCbz/main.cs
+++ OpenCbz/main.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpCompress.Archives.Zip;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
@@ -12,27 +13,42 @@ namespace OpenCbz
     {
         static void Main()
         {
-            ZipArchive zip;
-            MemoryStream memoryStream = new MemoryStream();
-            try
-            {
-                zip = ZipFile.Open("test.cbr", ZipArchiveMode.Read);
-                foreach (var entrie in zip.Entries)
-                {
-                    Console.WriteLine(entrie.FullName);
-                    Stream stream = entrie.Open();
-                    stream.CopyTo(memoryStream);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-
-
-
-
+            OpenZipToTempFiles("test.zip");
 
         }
+        static List<string> OpenZipToTempFiles(string fileZip)
+        {
+            List<string> fullNamesOfFiles = new List<string>();
+            ZipArchive zip;
+            try
+            {
+                zip = ZipArchive.Open(fileZip);
+                var entries = zip.Entries;
+                List<ZipArchiveEntry> listFiles = new List<ZipArchiveEntry>();
+                foreach (var entrie in entries)
+                {
+                    string fileName = entrie.ToString();
+                    Stream stream = entrie.OpenEntryStream();
+                    string fullName = Path.Combine(Path.GetTempPath(), fileName);
+                    if (Path.GetFileName(fullName) == "") continue;
+                    fullNamesOfFiles.Add(fullName);
+                    Directory.CreateDirectory(Path.GetDirectoryName(fullName));
+                    using (FileStream fileStream = new FileStream(fullName, FileMode.Create, FileAccess.Write))
+                    {
+                        stream.CopyTo(fileStream);
+                    }
+                }
+                zip.Dispose();
+            }
+            catch
+            {
+                throw;
+            }
+            return fullNamesOfFiles;
+        }
+
+
     }
+
 }
+
