b8fcdbcfe92e0be3bad368faaa7e14b2bfa10d62
newtom69 <thomas@vuille.fr>
2019-05-27 17:37:25 +0200

    more clean code

--- ImageProcessBack/MimeType.cs
+++ ImageProcessBack/MimeType.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Mime;
 using System.Runtime.InteropServices;
 
@@ -42,36 +43,25 @@ class MimeType
             IntPtr mimeTypePtr = new IntPtr(mimetype);
             string mime = Marshal.PtrToStringUni(mimeTypePtr);
             Marshal.FreeCoTaskMem(mimeTypePtr);
-            // TODO clean code
+            // add mimes not known by FindMimeFromData
             if (mime == genericMime)
             {
-                byte b0 = buffer[0];
-                byte b1 = buffer[1];
-                byte b2 = buffer[2];
-                byte b3 = buffer[3];
-                byte b4 = buffer[4];
-                byte b5 = buffer[5];
-                byte b6 = buffer[6];
-                byte b7 = buffer[7];
-                byte b8 = buffer[8];
-                byte b9 = buffer[9];
-                byte b10 = buffer[10];
-                byte b11 = buffer[11];
-                byte b16 = buffer[16];
-                byte b17 = buffer[17];
-                byte b18 = buffer[18];
-                byte b19 = buffer[19];
-                byte b20 = buffer[20];
-                byte b21 = buffer[21];
-                byte b22 = buffer[22];
-                if ( b0 == 'R' && b1 == 'I' && b2 == 'F' && b3 == 'F' &&
-                    b8 == 'W' && b9 == 'E' && b10 == 'B' && b11 == 'P')
+                ArraySegment<byte> buffer_0_3 = new ArraySegment<byte>(buffer, 0, 4);
+                ArraySegment<byte> buffer_4_7 = new ArraySegment<byte>(buffer, 4, 4);
+                ArraySegment<byte> buffer_8_11 = new ArraySegment<byte>(buffer, 8, 4);
+                ArraySegment<byte> buffer_16_22 = new ArraySegment<byte>(buffer, 16, 7);
+
+                byte[] array_RIFF = { (byte)'R', (byte)'I', (byte)'F', (byte)'F' };
+                byte[] array_WEBP = { (byte)'W', (byte)'E', (byte)'B', (byte)'P' };
+                byte[] array_jP = { (byte)'j', (byte)'P', (byte)' ', (byte)' ' };
+                byte[] array_ftypjp2 = { (byte)'f', (byte)'t', (byte)'y', (byte)'p', (byte)'j', (byte)'p', (byte)'2' };
+
+                if (buffer_0_3.SequenceEqual(array_RIFF) && buffer_8_11.SequenceEqual(array_WEBP))
                 {
                     mime = webpMime;
                     return mime;
                 }
-                if (b4 == 'j' && b5 == 'P' && b6 == ' ' && b7 == ' ' &&
-                    b16 == 'f' && b17 == 't' && b18 == 'y' && b19 == 'p' && b20 == 'j' && b21 == 'p' && b22 == '2')
+                if (buffer_4_7.SequenceEqual(array_jP) && buffer_16_22.SequenceEqual(array_ftypjp2))
                 {
                     mime = jpeg2000Mime;
                     return mime;
