90db0c213da29014667bbe0b121ad17e540ae205
newtom69 <thomas@vuille.fr>
2019-06-01 17:38:50 +0200

    change ManagePdf directory name

--- ImageProcessBack/ImageProcessBack.cs
+++ ImageProcessBack/ImageProcessBack.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 using ImageProcessLib;
-using SharpCompress.Archives;
 using ManageCompressedFile;
 
 /// <summary>
